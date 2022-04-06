463c318ba72b6d159c1e44b108ad6523252e3bea
34644 <34644@MSI>
2022-04-07 00:36:23 +0200

    El resto que quedaba

---
+++ IS-ITER/src/main/java/gui/CloseEventGUI.java
@@ -0,0 +1,319 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Vector;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+import com.toedter.calendar.JCalendar;
+
+import businessLogic.BLFacade;
+import configuration.UtilDate;
+import domain.Event;
+import domain.User;
+
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import java.awt.Dimension;
+
+public class CloseEventGUI extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	DefaultComboBoxModel<Event> modelEvents = new DefaultComboBoxModel<Event>();
+
+	private JLabel jLabelListOfEvents = new JLabel(ResourceBundle.getBundle("Etiquetas").getString("ListEvents"));
+	private JLabel jLabelEventDate = new JLabel(ResourceBundle.getBundle("Etiquetas").getString("EventDate"));
+	private final JLabel jLabelEvents = new JLabel(ResourceBundle.getBundle("Etiquetas").getString("Events"));
+	private JCalendar jCalendar = new JCalendar();
+	private Calendar calendarAct = null;
+	private Calendar calendarAnt = null;
+	private JTable tableEvents = new JTable();
+	private DefaultTableModel tableModelEvents;
+
+	private JScrollPane scrollPaneEvents = new JScrollPane();
+
+	private JButton jButtonCloseEvent = new JButton(ResourceBundle.getBundle("Etiquetas").getString("CreateEvent")); //$NON-NLS-1$ //$NON-NLS-2$
+	private JButton jButtonClose = new JButton(ResourceBundle.getBundle("Etiquetas").getString("Close"));
+	private JLabel jLabelMsg = new JLabel();
+	private JLabel jLabelError = new JLabel();
+	private Event eve;
+	private Vector<Date> datesWithEventsCurrentMonth = new Vector<Date>();
+	private String[] columnNamesEvents = new String[] { ResourceBundle.getBundle("Etiquetas").getString("EventN"),
+			ResourceBundle.getBundle("Etiquetas").getString("Event"),
+
+	};
+	BLFacade facade = MainGUI.getBusinessLogic();
+	/**
+	 * Create the frame.
+	 */
+
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					CreateEventGUI frame = new CreateEventGUI(null);
+//					frame.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
+
+	public CloseEventGUI(Vector<domain.Event> v) {
+
+		try {
+			jbInit(v);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void jbInit(Vector<domain.Event> v) throws Exception {
+		
+		
+
+		this.getContentPane().setLayout(null);
+		this.setSize(new Dimension(697, 370));
+		this.setTitle(ResourceBundle.getBundle("Etiquetas").getString("CreateEvent"));
+		jLabelListOfEvents.setBounds(new Rectangle(290, 18, 277, 20));
+
+		jCalendar.setBounds(new Rectangle(40, 50, 225, 150));
+		scrollPaneEvents.setBounds(new Rectangle(25, 44, 346, 116));
+
+		
+		jButtonClose.setBounds(new Rectangle(310, 259, 215, 30));
+		jButtonClose.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				jButtonClose_actionPerformed(e);
+			}
+		});
+
+		jLabelMsg.setBounds(new Rectangle(100, 300, 380, 20));
+		jLabelMsg.setForeground(Color.red);
+		// jLabelMsg.setSize(new Dimension(305, 20));
+
+		jLabelError.setBounds(new Rectangle(100, 300, 380, 20));
+		jLabelError.setForeground(Color.red);
+
+		this.getContentPane().add(jLabelMsg, null);
+		this.getContentPane().add(jLabelError, null);
+
+		this.getContentPane().add(jButtonClose, null);
+		this.getContentPane().add(jButtonCloseEvent, null);
+		this.getContentPane().add(jLabelListOfEvents, null);
+
+		this.getContentPane().add(jCalendar, null);
+
+		
+		datesWithEventsCurrentMonth = facade.getEventsMonth(jCalendar.getDate());
+		paintDaysWithEvents(jCalendar, datesWithEventsCurrentMonth);
+
+		jLabelEventDate.setBounds(new Rectangle(40, 15, 140, 25));
+		jLabelEventDate.setBounds(40, 16, 140, 25);
+		getContentPane().add(jLabelEventDate);
+
+		// Code for JCalendar
+		this.jCalendar.addPropertyChangeListener(new PropertyChangeListener() {
+			public void propertyChange(PropertyChangeEvent propertychangeevent) {
+//					this.jCalendar.addPropertyChangeListener(new PropertyChangeListener() {
+//						public void propertyChange(PropertyChangeEvent propertychangeevent) {
+				if (propertychangeevent.getPropertyName().equals("locale")) {
+					jCalendar.setLocale((Locale) propertychangeevent.getNewValue());
+				} else if (propertychangeevent.getPropertyName().equals("calendar")) {
+					calendarAnt = (Calendar) propertychangeevent.getOldValue();
+					calendarAct = (Calendar) propertychangeevent.getNewValue();
+					System.out.println("calendarAnt: " + calendarAnt.getTime());
+					System.out.println("calendarAct: " + calendarAct.getTime());
+					DateFormat dateformat1 = DateFormat.getDateInstance(1, jCalendar.getLocale());
+
+					int monthAnt = calendarAnt.get(Calendar.MONTH);
+					int monthAct = calendarAct.get(Calendar.MONTH);
+					if (monthAct != monthAnt) {
+						if (monthAct == monthAnt + 2) {
+							// Si en JCalendar estÃ¡ 30 de enero y se avanza al mes siguiente,
+							// devolverÃ­a 2 de marzo (se toma como equivalente a 30 de febrero)
+							// Con este cÃ³digo se dejarÃ¡ como 1 de febrero en el JCalendar
+							calendarAct.set(Calendar.MONTH, monthAnt + 1);
+							calendarAct.set(Calendar.DAY_OF_MONTH, 1);
+						}
+
+						jCalendar.setCalendar(calendarAct);
+
+						BLFacade facade = MainGUI.getBusinessLogic();
+
+						datesWithEventsCurrentMonth = facade.getEventsMonth(jCalendar.getDate());
+					}
+
+					paintDaysWithEvents(jCalendar, datesWithEventsCurrentMonth);
+
+					// Date firstDay = UtilDate.trim(new
+					// Date(jCalendar.getCalendar().getTime().getTime()));
+					Date firstDay = UtilDate.trim(calendarAct.getTime());
+					try {
+					tableModelEvents.setDataVector(null, columnNamesEvents);
+					tableModelEvents.setColumnCount(3); // another column added to allocate ev objects
+
+					BLFacade facade = MainGUI.getBusinessLogic();
+
+					Vector<domain.Event> events = facade.getEvents(firstDay);
+
+					if (events.isEmpty())
+						jLabelEvents.setText(ResourceBundle.getBundle("Etiquetas").getString("NoEvents") + ": "
+								+ dateformat1.format(calendarAct.getTime()));
+					else
+						jLabelEvents.setText(ResourceBundle.getBundle("Etiquetas").getString("Events") + ": "
+								+ dateformat1.format(calendarAct.getTime()));
+					for (domain.Event ev : events) {
+						Vector<Object> row = new Vector<Object>();
+
+						System.out.println("Events " + ev);
+
+						row.add(ev.getEventNumber());
+						row.add(ev.getDescription());
+						row.add(ev); // ev object added in order to obtain it with tableModelEvents.getValueAt(i,2)
+						tableModelEvents.addRow(row);
+					}
+					tableEvents.getColumnModel().getColumn(0).setPreferredWidth(25);
+					tableEvents.getColumnModel().getColumn(1).setPreferredWidth(268);
+					tableEvents.getColumnModel().removeColumn(tableEvents.getColumnModel().getColumn(2)); 
+					
+					} catch (Exception e1) {
+
+						jLabelError.setText(e1.getMessage());
+					}
+
+				}
+			}
+		});
+		scrollPaneEvents.setBounds(new Rectangle(292, 50, 346, 150));
+
+		tableEvents.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int i = tableEvents.getSelectedRow();
+				domain.Event ev = (domain.Event) tableModelEvents.getValueAt(i, 2);
+				facade.eliminarEvent(ev);
+			}
+		});
+		scrollPaneEvents.setViewportView(tableEvents);
+		tableModelEvents = new DefaultTableModel(null, columnNamesEvents);
+
+		tableEvents.setModel(tableModelEvents);
+		tableEvents.getColumnModel().getColumn(0).setPreferredWidth(25);
+		tableEvents.getColumnModel().getColumn(1).setPreferredWidth(268);
+		
+		this.getContentPane().add(scrollPaneEvents, null);
+
+		jButtonCloseEvent.setBounds(new Rectangle(40, 259, 215, 30));
+		jButtonCloseEvent.setText(ResourceBundle.getBundle("Etiquetas").getString("Close"));
+		
+		JScrollPane scrollPaneEvents_1 = new JScrollPane();
+		scrollPaneEvents_1.setBounds(new Rectangle(292, 50, 346, 150));
+		scrollPaneEvents_1.setBounds(290, 50, 346, 150);
+		getContentPane().add(scrollPaneEvents_1);
+		jButtonCloseEvent.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				facade.eliminarEvent(eve);
+			}
+		});
+		
+	}
+
+	public static void paintDaysWithEvents(JCalendar jCalendar, Vector<Date> datesWithEventsCurrentMonth) {
+		// For each day with events in current month, the background color for that day
+		// is changed.
+
+		Calendar calendar = jCalendar.getCalendar();
+
+		int month = calendar.get(Calendar.MONTH);
+		int today = calendar.get(Calendar.DAY_OF_MONTH);
+		int year = calendar.get(Calendar.YEAR);
+
+		calendar.set(Calendar.DAY_OF_MONTH, 1);
+		int offset = calendar.get(Calendar.DAY_OF_WEEK);
+
+		if (Locale.getDefault().equals(new Locale("es")))
+			offset += 4;
+		else
+			offset += 5;
+
+		for (Date d : datesWithEventsCurrentMonth) {
+
+			calendar.setTime(d);
+			System.out.println(d);
+
+			// Obtain the component of the day in the panel of the DayChooser of the
+			// JCalendar.
+			// The component is located after the decorator buttons of "Sun", "Mon",... or
+			// "Lun", "Mar"...,
+			// the empty days before day 1 of month, and all the days previous to each day.
+			// That number of components is calculated with "offset" and is different in
+			// English and Spanish
+//				    		  Component o=(Component) jCalendar.getDayChooser().getDayPanel().getComponent(i+offset);; 
+			Component o = (Component) jCalendar.getDayChooser().getDayPanel()
+					.getComponent(calendar.get(Calendar.DAY_OF_MONTH) + offset);
+			o.setBackground(Color.CYAN);
+		}
+
+		calendar.set(Calendar.DAY_OF_MONTH, today);
+		calendar.set(Calendar.MONTH, month);
+		calendar.set(Calendar.YEAR, year);
+		
+
+	}
+
+	private void jButtonCloseEvent_actionPerformed(ActionEvent e) {
+		// domain.Event event = ((domain.Event) jComboBoxEvents.getSelectedItem());
+
+		try {
+			jLabelError.setText("");
+			jLabelMsg.setText("");
+			Date eventDate = jCalendar.getDate();
+			BLFacade facade = MainGUI.getBusinessLogic();
+
+			User user = facade.getUserLogged();
+//			Pronostico pronostico = facade.getStorePronostico();
+			facade.closeEvent(eventDate);
+			facade.ajustWallet(eventDate, user);
+
+			jLabelMsg.setText(ResourceBundle.getBundle("Etiquetas").getString("QueryClosed"));
+
+			// } catch (EventFinished e1) {
+			// jLabelMsg.setText(ResourceBundle.getBundle("Etiquetas").getString("ErrorEventHasFinished")
+			// + ": "
+			// + event.getDescription());
+			// } catch (QuestionAlreadyExist e1) {
+			// jLabelMsg.setText(ResourceBundle.getBundle("Etiquetas").getString("ErrorQueryAlreadyExist"));
+			// } catch (java.lang.NumberFormatException e1) {
+			// jLabelError.setText(ResourceBundle.getBundle("Etiquetas").getString("ErrorNumber"));
+		} catch (Exception e1) {
+
+			e1.printStackTrace();
+
+		}
+	}
+	
+	private void jButtonClose_actionPerformed(ActionEvent e) {
+		this.setVisible(false);
+	}
+}
---
+++ IS-ITER/src/main/java/gui/CreatePronGUI.java
@@ -0,0 +1,311 @@
+package gui;
+
+import businessLogic.BLFacade;
+
+import configuration.UtilDate;
+
+import com.toedter.calendar.JCalendar;
+
+import domain.Pronostico;
+import domain.Question;
+import domain.User;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.beans.*;
+import java.text.DateFormat;
+import java.util.*;
+
+import javax.swing.table.DefaultTableModel;
+
+public class CreatePronGUI extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	private final JLabel jLabelEventDate = new JLabel(ResourceBundle.getBundle("Etiquetas").getString("EventDate"));
+	private final JLabel jLabelQueries = new JLabel(ResourceBundle.getBundle("Etiquetas").getString("Queries"));
+	private final JLabel jLabelEvents = new JLabel(ResourceBundle.getBundle("Etiquetas").getString("Events"));
+
+	private JButton jButtonClose = new JButton(ResourceBundle.getBundle("Etiquetas").getString("Close"));
+
+	// Code for JCalendar
+	private JCalendar jCalendar1 = new JCalendar();
+	private Calendar calendarAnt = null;
+	private Calendar calendarAct = null;
+	private JScrollPane scrollPaneEvents = new JScrollPane();
+	private JScrollPane scrollPaneQueries = new JScrollPane();
+
+	private Vector<Date> datesWithEventsCurrentMonth = new Vector<Date>();
+
+	private JTable tableEvents = new JTable();
+	private JTable tableQueries = new JTable();
+
+	private DefaultTableModel tableModelEvents;
+	private DefaultTableModel tableModelQueries;
+	private Question ques;
+
+	private JButton createPronButton = new JButton(ResourceBundle.getBundle("Etiquetas").getString("Pronostico")); //$NON-NLS-1$ //$NON-NLS-2$
+	// Local variables
+	private domain.Event evento;
+	BLFacade facade = MainGUI.getBusinessLogic();
+//	private String[] res;
+
+	private String[] columnNamesEvents = new String[] { ResourceBundle.getBundle("Etiquetas").getString("EventN"),
+			ResourceBundle.getBundle("Etiquetas").getString("Event"),
+
+	};
+	private String[] columnNamesQueries = new String[] { ResourceBundle.getBundle("Etiquetas").getString("QueryN"),
+			ResourceBundle.getBundle("Etiquetas").getString("Query")
+
+	};
+	private String[] columnNamesProns = new String[] { ResourceBundle.getBundle("Etiquetas").getString("QueryN"),
+			ResourceBundle.getBundle("Etiquetas").getString("PronN")
+
+	};
+
+	private final JLabel errorLabel = new JLabel(); // $NON-NLS-1$ //$NON-NLS-2$
+	private JTextField pronField;
+	private Question a;
+
+	public CreatePronGUI() {
+		try {
+			jbInit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void jbInit() throws Exception {
+
+		this.getContentPane().setLayout(null);
+		this.setSize(new Dimension(772, 564));
+		this.setTitle(ResourceBundle.getBundle("Etiquetas").getString("QueryQueries"));
+
+		jLabelEventDate.setBounds(new Rectangle(40, 15, 140, 25));
+		jLabelQueries.setBounds(40, 210, 406, 14);
+		jLabelEvents.setBounds(295, 19, 259, 16);
+
+		this.getContentPane().add(jLabelEventDate, null);
+		this.getContentPane().add(jLabelQueries);
+		this.getContentPane().add(jLabelEvents);
+
+		jButtonClose.setBounds(new Rectangle(138, 443, 130, 30));
+
+		jButtonClose.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				jButton2_actionPerformed(e);
+			}
+		});
+
+		this.getContentPane().add(jButtonClose, null);
+
+		jCalendar1.setBounds(new Rectangle(40, 50, 225, 150));
+
+		BLFacade facade = MainGUI.getBusinessLogic();
+		datesWithEventsCurrentMonth = facade.getEventsMonth(jCalendar1.getDate());
+		CreateQuestionGUI.paintDaysWithEvents(jCalendar1, datesWithEventsCurrentMonth);
+
+		// Code for JCalendar
+		this.jCalendar1.addPropertyChangeListener(new PropertyChangeListener() {
+			public void propertyChange(PropertyChangeEvent propertychangeevent) {
+
+				if (propertychangeevent.getPropertyName().equals("locale")) {
+					jCalendar1.setLocale((Locale) propertychangeevent.getNewValue());
+				} else if (propertychangeevent.getPropertyName().equals("calendar")) {
+					calendarAnt = (Calendar) propertychangeevent.getOldValue();
+					calendarAct = (Calendar) propertychangeevent.getNewValue();
+					DateFormat dateformat1 = DateFormat.getDateInstance(1, jCalendar1.getLocale());
+//					jCalendar1.setCalendar(calendarAct);
+					Date firstDay = UtilDate.trim(new Date(jCalendar1.getCalendar().getTime().getTime()));
+
+					int monthAnt = calendarAnt.get(Calendar.MONTH);
+					int monthAct = calendarAct.get(Calendar.MONTH);
+
+					if (monthAct != monthAnt) {
+						if (monthAct == monthAnt + 2) {
+							// Si en JCalendar está 30 de enero y se avanza al mes siguiente, devolvería 2
+							// de marzo (se toma como equivalente a 30 de febrero)
+							// Con este código se dejará como 1 de febrero en el JCalendar
+							calendarAct.set(Calendar.MONTH, monthAnt + 1);
+							calendarAct.set(Calendar.DAY_OF_MONTH, 1);
+						}
+
+						jCalendar1.setCalendar(calendarAct);
+
+						BLFacade facade = MainGUI.getBusinessLogic();
+
+						datesWithEventsCurrentMonth = facade.getEventsMonth(jCalendar1.getDate());
+					}
+
+					CreateQuestionGUI.paintDaysWithEvents(jCalendar1, datesWithEventsCurrentMonth);
+
+					try {
+						tableModelEvents.setDataVector(null, columnNamesEvents);
+						tableModelEvents.setColumnCount(3); // another column added to allocate ev objects
+
+						BLFacade facade = MainGUI.getBusinessLogic();
+
+						Vector<domain.Event> events = facade.getEvents(firstDay);
+
+						if (events.isEmpty())
+							jLabelEvents.setText(ResourceBundle.getBundle("Etiquetas").getString("NoEvents") + ": "
+									+ dateformat1.format(calendarAct.getTime()));
+						else
+							jLabelEvents.setText(ResourceBundle.getBundle("Etiquetas").getString("Events") + ": "
+									+ dateformat1.format(calendarAct.getTime()));
+						for (domain.Event ev : events) {
+							Vector<Object> row = new Vector<Object>();
+
+							System.out.println("Events " + ev);
+
+							row.add(ev.getEventNumber());
+							row.add(ev.getDescription());
+							row.add(ev); // ev object added in order to obtain it with tableModelEvents.getValueAt(i,2)
+							tableModelEvents.addRow(row);
+						}
+						tableEvents.getColumnModel().getColumn(0).setPreferredWidth(25);
+						tableEvents.getColumnModel().getColumn(1).setPreferredWidth(268);
+						tableEvents.getColumnModel().removeColumn(tableEvents.getColumnModel().getColumn(2)); // not
+																												// shown
+																												// in
+																												// JTable
+					} catch (Exception e1) {
+
+						jLabelQueries.setText(e1.getMessage());
+					}
+
+				}
+			}
+		});
+
+		this.getContentPane().add(jCalendar1, null);
+
+		scrollPaneEvents.setBounds(new Rectangle(292, 50, 456, 150));
+		scrollPaneQueries.setBounds(new Rectangle(40, 234, 406, 116));
+
+		tableEvents.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int i = tableEvents.getSelectedRow();
+				domain.Event ev = (domain.Event) tableModelEvents.getValueAt(i, 2);// obtain ev object
+				evento = ev;
+				Vector<Question> queries = ev.getQuestions();
+
+				tableModelQueries.setDataVector(null, columnNamesQueries);
+
+				if (queries.isEmpty())
+					jLabelQueries.setText(
+							ResourceBundle.getBundle("Etiquetas").getString("NoQueries") + ": " + ev.getDescription());
+				else
+					jLabelQueries.setText(ResourceBundle.getBundle("Etiquetas").getString("SelectedEvent") + " "
+							+ ev.getDescription());
+
+				for (domain.Question q : queries) {
+					Vector<Object> row = new Vector<Object>();
+
+					row.add(q.getQuestionNumber());
+					row.add(q.getQuestion());
+					tableModelQueries.addRow(row);
+				}
+				tableQueries.getColumnModel().getColumn(0).setPreferredWidth(25);
+				tableQueries.getColumnModel().getColumn(1).setPreferredWidth(268);
+			}
+		});
+
+		scrollPaneEvents.setViewportView(tableEvents);
+		tableModelEvents = new DefaultTableModel(null, columnNamesEvents);
+
+		tableEvents.setModel(tableModelEvents);
+		tableEvents.getColumnModel().getColumn(0).setPreferredWidth(25);
+		tableEvents.getColumnModel().getColumn(1).setPreferredWidth(268);
+		tableQueries.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int i = tableEvents.getSelectedRow();
+				int u = tableQueries.getSelectedRow();
+				domain.Event ev = (domain.Event) tableModelEvents.getValueAt(i, 2);
+				Vector<Question> queries = ev.getQuestions();
+				for (domain.Question q : queries) {
+
+					if (q.getQuestion().equals(tableQueries.getValueAt(u, 1))) {
+						domain.Question a = q;
+						ques=q;
+						a.setQuestion(tableQueries.getValueAt(i, 1).toString());
+
+						break;
+					}
+				}
+
+			}
+		});
+
+		scrollPaneQueries.setViewportView(tableQueries);
+		tableModelQueries = new DefaultTableModel(null, columnNamesQueries);
+
+		tableQueries.setModel(tableModelQueries);
+		tableQueries.getColumnModel().getColumn(0).setPreferredWidth(25);
+		tableQueries.getColumnModel().getColumn(1).setPreferredWidth(268);
+
+		this.getContentPane().add(scrollPaneEvents, null);
+		this.getContentPane().add(scrollPaneQueries, null);
+		errorLabel.setBounds(138, 416, 443, 13);
+
+		getContentPane().add(errorLabel);
+
+		pronField = new JTextField();
+		// $NON-NLS-1$ //$NON-NLS-2$
+		pronField.setBounds(60, 387, 386, 19);
+		getContentPane().add(pronField);
+		pronField.setColumns(10);
+		
+		createPronButton.setText(ResourceBundle.getBundle("Etiquetas").getString("Guardar"));
+		createPronButton.setBounds(291, 443, 140, 30);
+		getContentPane().add(createPronButton);
+		
+		JLabel lblRespuestacorrecta = new JLabel(ResourceBundle.getBundle("Etiquetas").getString("RespuestaCorrecta")); //$NON-NLS-1$ //$NON-NLS-2$
+		lblRespuestacorrecta.setBounds(60, 355, 259, 16);
+		getContentPane().add(lblRespuestacorrecta);
+		
+//		createPronButton.setVisible(true);
+
+//		tableQueries.addMouseListener(new MouseAdapter() {
+//			@Override
+//			public void mouseClicked(MouseEvent e) {
+//				int i = tableEvents.getSelectedRow();
+//				int u = tableQueries.getSelectedRow();
+//				domain.Event ev = (domain.Event) tableModelEvents.getValueAt(i, 2);
+//				Vector<Question> queries = ev.getQuestions();
+//				for (domain.Question q : queries) {
+//
+//					if(q.getQuestion().equals(tableQueries.getValueAt(u, 1))) {
+//						a = q;
+//						a.setQuestion(tableQueries.getValueAt(i, 1).toString());
+//						
+//						break;
+//					}
+//				}							
+//
+//			}
+//		});
+
+		createPronButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (pronField.getText() == null) {
+					errorLabel.setText("Introduzca una respuesta");
+				} else {
+					String prons = pronField.getText();
+					Pronostico pronCorrecto = new Pronostico(ques, prons);
+
+					facade.storePronosticoVerdadero(pronCorrecto, ques, evento);
+
+					errorLabel.setText("Pronostico creado con exito");
+				}
+			}
+		});
+
+	}
+
+	private void jButton2_actionPerformed(ActionEvent e) {
+		this.setVisible(false);
+	}
+}
